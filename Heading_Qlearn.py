4833e7232defd7190b714cd9f852be8c97b43b31
zjlaw <zjlaw@outlook.com>
2023-04-22 07:56:03 -0400

    Removed extra spaces and commented code

--- Heading_Qlearn.py
+++ Heading_Qlearn.py
@@ -2,13 +2,11 @@ import pyrealsense2.pyrealsense2 as rs
 import numpy as np
 import math as m
 from adafruit_servokit import ServoKit
-# import multiprocessing
 import time
 from datetime import datetime as dt
 import matplotlib.pyplot as plt
 import pickle
 import xlsxwriter as xl
-# from termios import
 
 steps = 37
 neg_vel = -3
@@ -29,11 +27,10 @@ ang_lim = 21
 omega_lim = 4
 alpha = .2
 gamma = .9
-epsilon = 1
+epsilon_decay = 200
 vel_space = np.linspace(neg_vel, pos_vel, steps)
 heading_space = np.linspace(neg_ang, pos_ang, steps)
 omega_space = np.linspace(neg_omega, pos_omega, steps)
-# actions = np.linspace(motor_ccw, motor_cw, action_step)
 actions = (-1, -0.81, -.62, -0.43, -0.23, -0.04, 0.15, 0.29, 0.43, 0.58, 0.72, 0.86, 1)
 vel_factor = 5
 heading_factor = .067
@@ -45,6 +42,7 @@ channel = 16
 time_step = 0.5
 startup = 5
 colors = np.linspace(0, iterations, iterations + 1)
+output = 5
 pause = 20
 
 states = []
@@ -52,7 +50,6 @@ for v in range(len(vel_space)):
     for h in range(len(heading_space)):
         for o in range(len(omega_space)):
             states.append((v,h,o))
-
 
 def get_obs(pipe):
     # Wait for the next set of frames from the camera
@@ -67,7 +64,6 @@ def get_obs(pipe):
         y = data.rotation.x
         z = -data.rotation.y
         
-        
         # pitch = -np.arcsin(2.0 * (x * z - w * y)) * 180 / (np.pi)
         # roll  =  np.arctan2(2.0 * (w*x + y*z), w*w - x*x - y*y + z*z) * 180.0 / (np.pi)
         heading   =  np.arctan2(2.0 * (w*z + x*y), w*w + x*x - y*y - z*z) * 180.0 / (np.pi)
@@ -77,9 +73,6 @@ def get_obs(pipe):
         yvel = data.velocity.x
         omega = data.angular_velocity.y
         print('X position: {0:.7f}, Y position: {1:.7f}'.format(x_pos, y_pos))
-        # print("Frame #{}".format(pose.frame_number))
-        # print("RPY [deg]: Roll: {0:.7f}, Pitch: {1:.7f}, Yaw: {2:.7f}".format(roll, pitch, yaw))
-        # print("Velocity [m/s]: {0:.7f}" .format(xvel))
         x_data.append(x_pos)
         y_data.append(y_pos)
         xvel_data.append(xvel)
@@ -109,30 +102,36 @@ def get_reward(state_n, act):
         heading_reward = neg_reward
     return heading_reward
 
-def export_data(episode, time_now, xvel_data, yvel_data, heading_data, heading_reward, omega_data, action_taken):
+def export_data(episode, time_now, x_data, xvel_data, y_data, yvel_data, heading_data, heading_reward, omega_data, action_taken):
     wb = xl.Workbook('/home/pi/Zlawren1_Swimbot/Logs/Episode:_' + str(episode) + '_' + dt.now().strftime("%Y_%m_%d_%H_%M_%S") + '.xlsx')
     ws = wb.add_worksheet("Logged data")
     ws.write(0, 0, "Time Stamp")
-    ws.write(0, 1, "X Velocity [m/s]")
-    ws.write(0, 2, "Y Velocity [m/s]")
-    ws.write(0, 3, "Heading [Deg]")
-    ws.write(0, 4, "Heading Rewards")
-    ws.write(0, 5, "Omega [rad/s]")
-    ws.write(0, 6, "Action")
+    ws.write(0, 1, "X Position [m]")
+    ws.write(0, 2, "X Velocity [m/s]")
+    ws.write(0, 3, "Y Position [m]")
+    ws.write(0, 4, "Y Velocity [m/s]")
+    ws.write(0, 5, "Heading [Deg]")
+    ws.write(0, 6, "Heading Rewards")
+    ws.write(0, 7, "Omega [rad/s]")
+    ws.write(0, 8, "Action")
     for i, time in enumerate(time_now):
         ws.write(i+1, 0, time)
+    for i, x_pos in enumerate(x_data):
+        ws.write(i+1, 1, x_pos)
     for i, x_vel in enumerate(xvel_data):
-        ws.write(i+1, 1, x_vel)
+        ws.write(i+1, 2, x_vel)
+    for i, y_pos in enumerate(y_data):
+        ws.write(i+1, 3, y_pos)
     for i, y_vel in enumerate(yvel_data):
-        ws.write(i+1, 2, y_vel)
+        ws.write(i+1, 4, y_vel)
     for i, head in enumerate(heading_data):
-        ws.write(i+1, 3, head)
+        ws.write(i+1, 5, head)
     for i, head_rew in enumerate(heading_reward):
-        ws.write(i+1, 4, head_rew)
+        ws.write(i+1, 6, head_rew)
     for i, ome in enumerate(omega_data):
-        ws.write(i+1, 5, ome)
+        ws.write(i+1, 7, ome)
     for i, act in enumerate(action_taken):
-        ws.write(i+1, 6, act)
+        ws.write(i+1, 8, act)
     wb.close()
 
 servo = ServoKit(channels = channel).continuous_servo[0]
@@ -149,7 +148,18 @@ else:
     q_value = {}
     for state in states:
         for act in actions:
-            q_value[(state, act)] = np.random.random()    
+            q_value[(state, act)] = np.random.random()
+
+print("Do you want to load an epsilon value y/n? ")
+if input() == 'y':
+    print("What file path should I follow to find your epsilon?")
+    e_file = open(input(), 'rb')
+    epsilon = pickle.load(e_file)
+    e_file.close()
+else:
+    print("Epsilon will be set to 1 and decay linearly over the session.")
+    epsilon = 1
+
 total_reward = np.zeros(episodes)
 
 pipe = rs.pipeline()
@@ -211,8 +221,8 @@ for e in range(episodes):
         act_n = get_act(state_n)
         q_value[(state, act)] = q_value[(state, act)] + alpha * (reward + gamma * q_value[(state_n, act_n)] - q_value[(state, act)])
         xvel, heading, omega = xvel_n, heading_n, omega_n
-    if epsilon > 2 / episodes:
-        epsilon -= 2 / episodes
+    if epsilon - 2 / epsilon_decay > 0:
+        epsilon -= 2 / epsilon_decay
     else:
         epsilon = 0
 
@@ -231,11 +241,15 @@ for e in range(episodes):
     plt.close()
 
     heading_reward = track_reward
-    export_data(e, time_now, xvel_data, heading_data, omega_data, action_taken, heading_reward, track_reward)
+    export_data(e, time_now, x_data, xvel_data, y_data, yvel_data, heading_data, heading_reward, omega_data, action_taken)
+    if i % output == 0:
+        file_loc = open('/home/pi/Zlawren1_Swimbot/Q_Tables/' + time_stamp + '_episode:' + str(e) + '.file', 'wb')
+        pickle.dump(q_value, file_loc)
+        file_loc.close()
     
-    file_loc = open('/home/pi/Zlawren1_Swimbot/Q_Tables/' + time_stamp + '_episode:' + str(e) + '.file', 'wb')
-    pickle.dump(q_value, file_loc)
-    file_loc.close()
+        e_loc = open('Epsilon/' + time_stamp + '_episode:' + str(e) + '.file', 'wb')
+        pickle.dump(epsilon, e_loc)
+        e_loc.close()
     print('Are you ready for the next episode: y/n?')
     if input() == 'y':
         pass
